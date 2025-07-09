f84b2ff6bd46901857a69511cbb0cebe8c28ec1f
roshansinhabahu <roshansinhabahu@gmail.com>
2025-07-10 00:39:34 +0530

    Update animation effects for Limitation and LoraInfo components

--- src/components/Limitation.js
+++ src/components/Limitation.js
@@ -1,7 +1,7 @@
 // Limitation.js
 function Limitation() {
   return (
-    <section id="technology" className="col-md-6 mb-4 mb-md-0 text-center py-5" data-aos="fade-up" data-aos-delay="100">
+    <section id="technology" className="col-md-6 mb-4 mb-md-0 text-center py-5" data-aos="fade-right" data-aos-delay="100">
       <h2 className="mb-4">Limitations of Existing Systems</h2>
       <i className="fas fa-exclamation-triangle fa-4x mb-5 text-primary"></i>
       <p>
--- src/components/LoraInfo.js
+++ src/components/LoraInfo.js
@@ -1,7 +1,7 @@
 // LoraInfo.js
 function LoraInfo() {
   return (
-    <div className="col-md-6 text-center" data-aos="fade-up" data-aos-delay="300">
+    <div className="col-md-6 text-center" data-aos="fade-left" data-aos-delay="300">
       <h2 className="mb-4">What is LoRa?</h2>
       <i className="fas fa-broadcast-tower fa-4x mb-5 text-primary"></i>
       <p>
